1b5ea534e4032265f8932ac5e7aa7c06ffcd4d66
kkww <kkww@kkww>
2017-05-04 16:55:20 +0900

    .

--- src/chat/ChatClientReceiveThread.java
+++ src/chat/ChatClientReceiveThread.java
@@ -1,5 +1,27 @@
 package chat;
 
-public class ChatClientReceiveThread {
+import java.io.BufferedReader;
+import java.io.IOException;
 
+public class ChatClientReceiveThread extends Thread {
+	private BufferedReader bufferedReader;
+	public ChatClientReceiveThread( BufferedReader bufferedReader ) {
+		this.bufferedReader = bufferedReader;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while( true ) {
+				String data = bufferedReader.readLine();
+				if( data == null ) {
+					break;
+				}
+
+				System.out.println( data );
+			}
+		} catch( IOException ex ) {
+			ChatClient.log( "error:" + ex );
+		}
+	}
 }
